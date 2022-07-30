4b7c421f676fedf1894c48dfd623469392e7fa85
Ezequiel Rivas <ezequiel@innovit.cl>
2022-07-30 18:17:26 -0300

    fix: asd

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  distDir: 'dist',
+  // sassOptions: {
+  //   includePaths: [path.join(__dirname, 'styles')],
+  // },
   images: {
     domains: ['assets.example.com'],
   },
